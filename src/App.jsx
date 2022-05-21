c00a00718c595e5bacb02a8600980580b37ce913
faridulhassan <faridul940@gmail.com>
2022-05-21 22:23:18 +0600

    project init and mui installed

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Button from '@mui/material/Button';
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+      <Button variant="contained">Hello World</Button>
+    </div>
+  )
+}
+
+export default App
