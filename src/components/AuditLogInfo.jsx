923f35a5f9bed18242df45f17ff386ff65854b20
faridulhassan <faridul940@gmail.com>
2022-05-23 12:16:47 +0600

    added audit log info component

---
+++ src/components/AuditLogInfo.jsx
@@ -0,0 +1,17 @@
+import Box from "@mui/material/Box";
+import Divider from "@mui/material/Divider";
+import Typography from "@mui/material/Typography";
+import moment from "moment";
+
+export default function AuditLogInfo(props) {
+    return (
+        <Box sx={{ bgcolor: "#F2F2F2", color: "text.secondary", px: 2, py: 1.5, mt: 2 }}>
+            <Typography variant="h6" component="h6" sx={{ fontWeight: "medium" }}>
+                Audit Log
+            </Typography>
+            <Divider sx={{ my: 1 }} />
+            {props.createdAt && <div>Created by Simon on {moment(props.createdAt).format("YYYY-MM-DD hh:mm:ss A")} </div>}
+            {props.updatedAt && <div>Updated by Nandita on {moment(props.updatedAt).format("YYYY-MM-DD hh:mm:ss A")}</div>}
+        </Box>
+    );
+}
